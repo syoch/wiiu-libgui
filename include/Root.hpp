cdf260ccd7e76f4e93e1dc4a026013aa1802175e
syoch <hacksyoch@gmail.com>
2021-03-06 23:16:05 +0900

    fix: root:drawText argument

--- include/Root.hpp
+++ include/Root.hpp
@@ -12,7 +12,7 @@ namespace GUI
         void draw_line(DrawPoint start, DrawPoint end) override;
         void draw_rect(DrawPoint start, DrawPoint end) override;
         void draw_triangle(DrawPoint A, DrawPoint B, DrawPoint C) override;
-        void draw_text(int row, int column, char *, Color color) override;
+        void draw_text(int row, int column, mstd::wstring text, Color color) override;
         void draw_translate(int x, int y) override;
 
     public:
