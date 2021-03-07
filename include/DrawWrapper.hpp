8148f938a5021d6e905a039f4a79ea7a0f999a81
syoch <hacksyoch@gmail.com>
2021-03-07 16:50:13 +0900

    style: move some funcdef

--- include/DrawWrapper.hpp
+++ include/DrawWrapper.hpp
@@ -11,12 +11,13 @@ namespace GUI
     void draw_begin();
     void draw_end();
     void draw_line(DrawPoint start, DrawPoint end);
-    void draw_rect(DrawPoint start, DrawPoint end);
     void draw_rect(DrawPoint A, DrawPoint B, DrawPoint C, DrawPoint D);
     void draw_triangle(DrawPoint A, DrawPoint B, DrawPoint C);
     void draw_text(int row, int column, mstd::wstring, Color color);
     void draw_textShadow(int row, int column, mstd::wstring, Color color);
     void draw_translate(float x, float y);
+    // this header/source file set function
+    void draw_rect(DrawPoint start, DrawPoint end);
     void _draw_translate(float x, float y);
 }
 
